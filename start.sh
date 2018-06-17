ea2c193ded89d9099ad61ef47e8945974fd1bc4a
sybdata <as15@wt.de>
2018-06-17 23:17:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exec /usr/bin/supervisord
+/opt/acestream/acestream.start >/dev/null 2>&1 &
+
+while true; do
+        sleep $1
+        rm -rf /opt/acestream/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
+done
